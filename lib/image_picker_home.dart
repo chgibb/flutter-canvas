ea9d62c522f66105c224019c83eb10f6d3c84bf6
chgibb <chris.gibb@outlook.com>
2019-07-02 15:25:12 -0400

    show images as listview of paintcontrollers

--- lib/image_picker_home.dart
+++ lib/image_picker_home.dart
@@ -5,17 +5,20 @@ import 'package:flutter/material.dart'
         AppBar,
         BuildContext,
         Center,
+        Divider,
         FloatingActionButton,
         Icon,
         Icons,
-        Image,
         Scaffold,
         State,
         StatefulWidget,
         Text,
         Widget;
+import 'package:flutter/widgets.dart';
 
 import 'package:flutter_canvas/image_picker.dart' show pickImageFromCamera;
+import 'package:flutter_canvas/new_painter_controller.dart';
+import 'package:painter2/painter2.dart';
 
 class ImagePickerHomePage extends StatefulWidget {
   @override
@@ -23,13 +26,12 @@ class ImagePickerHomePage extends StatefulWidget {
 }
 
 class _ImagePickerHomePageState extends State<ImagePickerHomePage> {
-  File _image;
-
+  List<PainterController> _images = new List();
   Future getImage() async {
     File image = await pickImageFromCamera();
 
     setState(() {
-      _image = image;
+      _images.add(newController(image));
     });
   }
 
@@ -39,9 +41,17 @@ class _ImagePickerHomePageState extends State<ImagePickerHomePage> {
       appBar: AppBar(
         title: Text('Image Picker Example'),
       ),
-      body: Center(
-        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: _images != null ? _images.length : 0,
+        itemBuilder: (BuildContext context, int index) {
+          return Center(
+            child: _images[index].backgroundImage,
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
+      // child: _image == null ? Text('No image selected.') : Image.file(_image),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage,
         tooltip: 'Pick Image',
