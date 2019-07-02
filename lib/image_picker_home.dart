4423b7e2cc3f3e3f04ad1db17db60bc058a6e18f
chgibb <chris.gibb@outlook.com>
2019-07-02 14:42:34 -0400

    factored out image picker poc and lint

---
+++ lib/image_picker_home.dart
@@ -0,0 +1,52 @@
+import 'dart:io' show File;
+
+import 'package:flutter/material.dart'
+    show
+        AppBar,
+        BuildContext,
+        Center,
+        FloatingActionButton,
+        Icon,
+        Icons,
+        Image,
+        Scaffold,
+        State,
+        StatefulWidget,
+        Text,
+        Widget;
+
+import 'package:flutter_canvas/image_picker.dart' show pickImageFromCamera;
+
+class ImagePickerHomePage extends StatefulWidget {
+  @override
+  _ImagePickerHomePageState createState() => _ImagePickerHomePageState();
+}
+
+class _ImagePickerHomePageState extends State<ImagePickerHomePage> {
+  File _image;
+
+  Future getImage() async {
+    File image = await pickImageFromCamera();
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
+      body: Center(
+        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
