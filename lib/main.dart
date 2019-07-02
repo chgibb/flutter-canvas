21b07b9e5726116fd47de0f307006d97d6616c3e
chgibb <chris.gibb@outlook.com>
2019-07-02 14:47:52 -0400

    c and p example

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_canvas/painter_home.dart';
 
 import 'image_picker_home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ImagePickerHomePage(),
+    home: ExamplePage(),
   ));
 }
 
