31f246f2c1d4c013dea39e132a1c73f7c83c69c0
chgibb <chris.gibb@outlook.com>
2019-07-03 10:54:28 -0400

    hold onto raw annotated bytes and generate thumbnail on each update

--- lib/image_controller.dart
+++ lib/image_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart' as Material;
+import 'package:image/image.dart';
 
 import 'package:painter2/painter2.dart';
 import 'package:image/image.dart' as Img;
@@ -8,10 +11,21 @@ class ImageController {
   Img.Image _thumb;
   Material.Image _origImage;
   Material.Image annotatedImage;
+  Uint8List annotatedBytes;
 
   ImageController(this._painterController) {
     _origImage = _painterController.backgroundImage;
     annotatedImage = _painterController.backgroundImage;
+  }
+
+  void updateAnnotation(Uint8List bytes) {
+    annotatedImage = Material.Image.memory(bytes);
+    annotatedBytes = bytes;
+    generateThumbnail();
+  }
+
+  void generateThumbnail() {
+    _thumb = copyResize(Img.Image.fromBytes(100,100, annotatedBytes),width: 10,height: 10);
   }
 
   PainterController get paintController => _painterController;
